4ad7dd56dac989059d51e95ba47f15543874e3f2
Sakshi Singh <130206553+sakshiji-here@users.noreply.github.com>
2025-07-15 16:31:20 +0530

    Initial Commit

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoute);
+
+app.listen(PORT, () => {
+  console.log("server is running on port  PORT:" + PORT);
+  connectDB();
+});
