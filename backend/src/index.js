1615aaa91ca41fcb6b2a5ba34b96ddbcc9dc68f4
Sakshi Singh <130206553+sakshiji-here@users.noreply.github.com>
2025-07-31 15:38:43 +0530

    updated index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  socketApp.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  socketApp.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
